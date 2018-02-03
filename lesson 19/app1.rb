bed0cb445a4036178aa25233fc880998867e9f81
maksimmanzhai <bender23max@gmail.com>
2018-02-04 00:43:50 +0300

    lesson 19 is finnish

---
+++ lesson 19/app1.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+
+get '/' do 
+	erb :index
+end
+
+post '/' do
+	@login = params[:aaa]
+	@password = params[:bbb]
+
+	if @login == "admin" && @password == "secret"
+		erb :welcome
+	else
+		erb :index
+	end
+end
