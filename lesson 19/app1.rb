8b4427a68b7b8bad5aa2057d6729fe620912cfc5
maksimmanzhai <bender23max@gmail.com>
2018-02-06 22:13:42 +0300

    lesson 20 is start

--- lesson 19/app1.rb
+++ lesson 19/app1.rb
@@ -4,13 +4,34 @@ get '/' do
 	erb :index
 end
 
+get '/contacts' do 
+	@title = 'Contacts'
+	@message = 'Phone number: 111222'
+	erb :message
+end
+
+get '/faq' do 
+	@title = 'FAQ'
+	@message = 'LOL KEK CHEBUREK'
+	erb :message
+end
+
+get '/something' do 
+	@title = 'Something'
+	@message = 'bla bla bla'
+	erb :message
+end
+
 post '/' do
 	@login = params[:aaa]
 	@password = params[:bbb]
 
-	if @login == "admin" && @password == "secret"
+	if @login == 'admin' && @password == '12345'
 		erb :welcome
+	elsif @login == 'admin' && @password == 'admin'
+		@message = 'Haha! Nice try! Access denied.'
 	else
+		@message = 'Access denied'
 		erb :index
 	end
 end
