66dfa4f03fb1cc3cf7a55a2a53d8661ac1ab1fc8
maksimmanzhai <bender23max@gmail.com>
2018-02-11 00:57:23 +0300

    lesson 20 is finnish

--- lesson 20/barbershop/barbershop.rb
+++ lesson 20/barbershop/barbershop.rb
@@ -1,7 +1,15 @@
+
+#3. sinatra format phone
+#4. sinatra date calendar
+
 require 'sinatra'
 
 get '/' do
 	erb :index
+end
+
+get '/admin' do
+	erb :admin
 end
 
 post '/' do
@@ -13,8 +21,23 @@ post '/' do
 	@message = "Dear #{@user_name}, phone #{@phone} we'll be waiting for you at #{@date_time}"
 	erb :message
 
-	f = File.open 'users.txt', 'a'
-	f.write "User: #{@user_name}, \tPhone: #{@phone}, \tDate and time: #{@date_time}\n"
-	f.close
+	@f = File.open 'users.txt', 'a'
+	@f.write "User: #{@user_name}, \tPhone: #{@phone}, \tDate and time: #{@date_time} \n"
+	@f.close
 	erb :message
+end
+
+post '/admin' do
+	@login = params[:login]
+	@password = params[:password]
+
+	if @login == 'admin' && @password == '12345'
+		@logfile = File.read("users.txt")
+		erb :list_of_users
+	elsif @login == 'admin' && @password == 'admin'
+		@message = 'Haha! Nice try! Access denied.'
+	else
+		@message = 'Access denied'
+		erb :index
+	end
 end
