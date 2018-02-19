0298a0377543447b231bcff4b531d62ee2ba5029
maksimmanzhai <bender23max@gmail.com>
2018-02-19 22:42:35 +0300

    homework for lesson 22

--- lesson 22/RubySchool_Lesson_22/app.rb
+++ lesson 22/RubySchool_Lesson_22/app.rb
@@ -1,17 +1,15 @@
-#1. add POST for visit
-#2. add data to ./public/users.txt
-#3. add /contacts with email, message
-#4. watch html-element textarea
+
 #5. all save contacts will add to ./public/contacts.txt
 #6. choise on /visit chairman with select
 #7. save data to file
 #8. access for admin with password
+
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-	erb "Hello!!! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb "Hello <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
 
 get '/about' do
@@ -20,8 +18,24 @@ end
 
 get '/visit' do
 	erb :visit
+
 end
 
 get '/contacts' do
 	erb :contacts
+end
+
+post '/visit' do
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@textarea = params[:textarea1]
+	@visits = File.open './public/users.txt', 'a'
+	@visits.write "User: #{@username},\tPhone: #{@phone},\tDate and time: #{@datetime}\n"
+	@visits.close
+	@message = "Dear #{@user_name}, phone #{@phone} we'll be waiting for you at #{@date_time}"
+	erb :message
+	@contacts = File.open './public/contacts.txt', 'a'
+	@contacts.write "#{@phone}\n"
+	@contacts.close
 end
