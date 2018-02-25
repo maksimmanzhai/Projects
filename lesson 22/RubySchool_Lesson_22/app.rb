5fd1b36c0f67ac1b74fac6ed5eb36496af31767b
maksimmanzhai <bender23max@gmail.com>
2018-02-25 21:26:00 +0300

    homewor of lesson 22 is finnish

--- lesson 22/RubySchool_Lesson_22/app.rb
+++ lesson 22/RubySchool_Lesson_22/app.rb
@@ -1,12 +1,13 @@
-
-#8. access for admin with password
-
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
 	erb "Hello <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+
+get '/admin' do
+	erb :admin
 end
 
 get '/about' do
@@ -32,7 +33,19 @@ post '/visit' do
 	@visits.write "User: #{@username},\tPhone: #{@phone},\tDate and time: #{@datetime}\tTextarea: #{@textarea}\tChairman: #{@chairman}\n"
 	@visits.close
 
-	@message = "Dear #{@user_name}, phone #{@phone} we'll be waiting for you at #{@datetime}. Yor chairman is #{@chairman}"
+	@message = "Dear #{@username}, phone #{@phone} we'll be waiting for you at #{@datetime}. Yor chairman is #{@chairman}"
 	erb :message
+end
 
+post '/admin' do
+	@login = params[:login]
+	@password = params[:password]
+
+	if @login == 'admin' && @password == '12345'
+		@logfile = File.read("./public/users.txt")
+		#erb :list_of_users
+	else
+		@message = 'Access denied'
+		erb :admin
+	end
 end
