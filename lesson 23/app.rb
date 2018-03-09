af3cde1e181a8878f0ffaf9bf03ecf3793410d6c
maksimmanzhai <bender23max@gmail.com>
2018-03-09 13:42:00 +0300

    add form

--- lesson 23/app.rb
+++ lesson 23/app.rb
@@ -1,1 +1,25 @@
 require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do 
+		erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+
+get '/about' do
+	erb :about
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	erb "OK, username is #{username}, #{phone}, @datetime, #{barber}"
+
+end
