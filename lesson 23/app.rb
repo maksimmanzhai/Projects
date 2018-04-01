8000e4f058b0feccc97d90426d181e261148db04
maksimmanzhai <bender23max@gmail.com>
2018-04-01 16:59:20 +0300

    lesson 24 is end

--- lesson 23/app.rb
+++ lesson 23/app.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pony'
 
 get '/' do 
 		erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
@@ -13,6 +14,14 @@ end
 get '/visit' do
 	erb :visit
 end
+
+get '/contacts' do
+	erb :contacts
+
+	Pony.options = { :from => 'noreply@example.com', :via => :smtp, :via_options => { :host => 'smtp.yourserver.com' } }
+	Pony.mail(:to => 'bender23max@gmail.com') # Sends mail to foo@bar from noreply@example.com using smtp
+end
+
 
 post '/visit' do
 
