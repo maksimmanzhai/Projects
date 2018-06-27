ed19c0627eeaac06336b774045a98b5dffb136c4
maksimmanzhai <bender23max@gmail.com>
2018-06-27 22:49:25 +0300

    keep barber choice on postback

---
+++ lesson_26/barbershop23/app.rb
@@ -0,0 +1,55 @@
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'pony'
+
+get '/' do 
+		erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+
+get '/about' do
+	erb :about
+end
+
+get '/visit' do
+	erb :visit
+end
+
+get '/contacts' do
+	erb :contacts
+	
+end
+
+post '/contact' do
+	
+end
+
+post '/visit' do
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	@color = params[:color]
+
+	@visits = File.open './public/users.sqlite', 'a'
+	@visits.write "User: #{@username},\tPhone: #{@phone},\tDate and time: #{@datetime}\tTextarea: #{@textarea}\tChairman: #{@chairman}\n"
+	@visits.close
+	
+	# хеш
+	hh = {:username => 'Enter name', 
+				:phone => 'Enter phone', 
+				:datetime => 'Enter date and time'}
+
+	@error = hh.select {|key,_| params[key] == ""}.values.join(", ")
+
+	if @error != ''
+		return erb :visit
+	end
+
+	erb "OK, username is #{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
+
+
+
+
+
+end
