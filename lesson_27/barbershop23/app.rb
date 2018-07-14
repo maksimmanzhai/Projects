d8453aebdf95b22a95a2df9a0e9cddc20b877ecc
maksimmanzhai <bender23max@gmail.com>
2018-07-14 14:34:19 +0300

    add table of Users in showusers

---
+++ lesson_27/barbershop23/app.rb
@@ -0,0 +1,83 @@
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sqlite3'
+
+
+def get_db
+	return SQLite3::Database.new "barbershop.db"
+end
+
+configure do
+	db = get_db
+	db.execute 'CREATE TABLE IF NOT EXISTS
+		"Users" 
+		(
+			"id" INTEGER PRIMARY KEY AUTOINCREMENT, 
+			"username" TEXT,
+			"phone" TEXT,
+			"datestamp" TEXT,
+			"barber" TEXT,
+			"color" TEXT
+		)'
+		db.close
+end
+
+get '/' do 
+		erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+
+get '/about' do
+	erb :about
+end
+
+get '/visit' do
+	erb :visit
+end
+
+get '/contacts' do
+	erb :contacts
+	
+end
+
+get '/showusers' do
+	erb :showusers
+end
+
+post '/visit' do
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	@color = params[:color]
+
+	@visits = File.open './public/users.txt', 'a'
+	@visits.write "User: #{@username},\tPhone: #{@phone},\tDate and time: #{@datetime}\tTextarea: #{@textarea}\tChairman: #{@chairman}\n"
+	@visits.close
+	
+	# хеш
+	hh = {:username => 'Enter name', 
+				:phone => 'Enter phone', 
+				:datetime => 'Enter date and time'}
+
+	@error = hh.select {|key,_| params[key] == ""}.values.join(", ")
+
+	if @error != ''
+		return erb :visit
+	end
+
+	db = get_db
+	db.execute 'INSERT INTO 
+		Users 
+			( 
+				username, 
+				phone,
+				datestamp,
+				barber,
+				color
+			)
+		values( ?, ?, ?, ?, ?)', [@username, @phone, @datetime, @barber, @color]
+
+	erb "OK, username is #{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
+
+end
