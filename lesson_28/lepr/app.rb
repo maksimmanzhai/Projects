f9d2af8368bb2fa5e13bdd9b1e1238e7739c37ac
maksimmanzhai <bender23max@gmail.com>
2018-08-12 15:30:16 +0300

    Show latest posts

--- lesson_28/lepr/app.rb
+++ lesson_28/lepr/app.rb
@@ -24,7 +24,8 @@ configure do
 end
 
 get '/' do
-	@results = @db.execute 'select * from Posts ordre by id desc'
+	init_db
+	@results = @db.execute 'select * from Posts order by id desc'
 	erb :index
 end
 
