a9e671a273079542670cb1c858455dcad9f1bec3
maksimmanzhai <bender23max@gmail.com>
2018-08-13 21:27:33 +0300

    Add details page

--- lesson_28/lepr/app.rb
+++ lesson_28/lepr/app.rb
@@ -45,3 +45,12 @@ post '/new' do
   redirect to "/"
   erb "You posted: #{content}"
 end
+
+get '/details/:post_id' do
+	post_id = params[:post_id]
+
+	results = @db.execute 'select * from Posts where id = ?', [post_id]
+	@row = results[0]
+
+	erb :details
+end
